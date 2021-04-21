32392097fb63c9ac9b1d3bb88454f35319c14ccd
N3UN3R <77114278+N3UN3R@users.noreply.github.com>
2021-04-21 20:41:56 +0200

    changed dictionary structure to nested dictionary

--- lokalerZusammenhang.py
+++ lokalerZusammenhang.py
@@ -3,8 +3,6 @@ from geopy.distance import geodesic
 
 
 
-#berechne die Entfernung der Prosumer Haushalte zu allen Haushalten
-
 """ this function calculates the distance from all prosumer-households to all households.
     As the distance from a prosumer household to itself would be zero this is set to 10000.
     
@@ -14,14 +12,15 @@ def calculate_distance_from_prosumers_to_allHouseholds():
 
     distance_from_prosumers_to_allHouseholds = {}
     for key1, coordinate1 in get_prosumer_coordinates('AssetListe.json').items():
+
+        #make every prosumerId a key of the distance_dictionary
+        distance_from_prosumers_to_allHouseholds[key1] = {}
         for key2, coordinate2 in get_allHouseholds_coordinates('AssetListe.json').items():
                 distance = float(geodesic(coordinate1, coordinate2).km)
-
-                matchedprosumerIds = tuple([key1, key2])
-                distance_from_prosumers_to_allHouseholds[matchedprosumerIds] = distance
+                distance_from_prosumers_to_allHouseholds[key1][key2] = distance
 
                 if key1 == key2:
-                    distance_from_prosumers_to_allHouseholds[matchedprosumerIds] = 100000
+                    distance_from_prosumers_to_allHouseholds[key1][key2] = 100000
 
     return distance_from_prosumers_to_allHouseholds
 
@@ -31,14 +30,16 @@ def calculate_distance_from_prosumers_to_allHouseholds():
     whether the distance in between to households is lower than this limit"""
 def calculate_price_reduction_of_local_trading():
 
-    localNeighbourDiscount = {}
-    for key, distance in calculate_distance_from_prosumers_to_allHouseholds().items():
-        if float(distance) <= float(4.5):
-            #2.07 is based on the Stromsteuer of an average price of 30 ct/kWh
-            localNeighbourDiscount[key] = float(2.07)
+    localNeighbourDiscount = calculate_distance_from_prosumers_to_allHouseholds().copy()
+    for prosumerId, consumers in localNeighbourDiscount.items():
+        for consumerId, distance in consumers.items():
 
-        else:
-            localNeighbourDiscount[key] = float(0)
+            if float(distance) <= float(4.5):
+                #2.07 is based on the Stromsteuer of an average price of 30 ct/kWh
+                localNeighbourDiscount[prosumerId][consumerId] = float(2.07)
+
+            else:
+                localNeighbourDiscount[prosumerId][consumerId] = float(0)
 
     return localNeighbourDiscount
 
