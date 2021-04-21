f0712cbbeb493c7d7eb905bd2d6e287cc7fd60a5
N3UN3R <77114278+N3UN3R@users.noreply.github.com>
2021-04-21 21:26:27 +0200

    removed key 'NO_METER_AVAILABLE'

--- getDataFromAssetListe.py
+++ getDataFromAssetListe.py
@@ -10,9 +10,12 @@ def read_asset_list(file):
         payload = json.load(json_data)
         asset_ids = []
         for household in payload['data']:
-            asset_ids.append(household['meterId'])
+            if household['meterId'] != 'NO_METER_AVAILABLE':
+                asset_ids.append(household['meterId'])
 
     return asset_ids
+
+print(read_asset_list('AssetListe.json'))
 
 """ This function reads in a file called prosumer_Ids.csv
     It contains a list of all meterIds that are prosumer households 
